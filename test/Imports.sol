0addfd1d2bd1c121b1504896023fafd75d2b3a13
Andrey Borzenkov <armoking32@gmail.com>
2025-06-17 19:50:30 +0400

    tests: WIP 0%

---
+++ test/Imports.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import "forge-std/Test.sol";
+
+import "../src/factories/Factory.sol";
+
+import "../src/hooks/BasicDepositHook.sol";
+import "../src/hooks/CompositeDepositHook.sol";
+import "../src/hooks/LidoStakingHook.sol";
+
+import "../src/libraries/FenwickTreeLibrary.sol";
+import "../src/libraries/MerkleHashingLibrary.sol";
+import "../src/libraries/PermissionsLibrary.sol";
+import "../src/libraries/SharesManagerFlagLibrary.sol";
+import "../src/libraries/SlotLibrary.sol";
+import "../src/libraries/TransferLibrary.sol";
+
+import "../src/modules/ACLModule.sol";
+import "../src/modules/BaseModule.sol";
+import "../src/modules/CallModule.sol";
+import "../src/modules/DepositModule.sol";
+import "../src/modules/RedeemModule.sol";
+import "../src/modules/RootVaultModule.sol";
+import "../src/modules/SharesModule.sol";
+import "../src/modules/SubvaultModule.sol";
+import "../src/modules/VerifierModule.sol";
+
+import "../src/oracles/Oracle.sol";
+
+import "../src/permissions/BitmaskVerifier.sol";
+import "../src/permissions/Verifier.sol";
+
+import "../src/queues/DepositQueue.sol";
+import "../src/queues/Queue.sol";
+import "../src/queues/RedeemQueue.sol";
+
+import "../src/shares/BasicSharesManager.sol";
+import "../src/shares/SharesManager.sol";
+import "../src/shares/TokenizedSharesManager.sol";
+
+import "../src/strategies/MulticallStrategy.sol";
+
+import "../src/vaults/RootVault.sol";
+import "../src/vaults/Subvault.sol";
---
+++ test/IntegrationTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import "./Imports.sol";
+
+contract Integration is Test {
+    function test() external {}
+}
