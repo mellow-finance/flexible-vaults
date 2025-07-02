00eea1e5bf320a342192247e0f6438ee3461c5e1
Anton Iants <yants.anton@gmail.com>
2025-07-02 12:10:02 +0400

    update unit tests for BitmaskVerifier contract

--- test/unit/permissions/BitmaskVerifier.t.sol
+++ test/unit/permissions/BitmaskVerifier.t.sol
@@ -3,26 +3,51 @@ pragma solidity 0.8.25;
 
 import "../../Imports.sol";
 
-contract Unit is Test {
-    function test() external {
-        BitmaskVerifier verifier = new BitmaskVerifier();
-        address who = vm.createWallet("caller").addr;
-        address where = vm.createWallet("target").addr;
-        uint256 value = 100 ether;
-        bytes memory callData = abi.encodeWithSignature("testFunction(uint256)", 42);
+contract BitmaskVerifierTest is Test {
+    address who = vm.createWallet("caller").addr;
+    address where = vm.createWallet("target").addr;
+    bytes callData = abi.encodeWithSignature("testFunction(uint256)", 42);
+    bytes wrongCallData = abi.encodeWithSignature("testFunction1(uint256,uint256)", 42, 43);
 
-        bytes memory bitmask = abi.encode(
+    function testValid() external {
+        BitmaskVerifier verifier = new BitmaskVerifier();
+        uint256 value = 100 ether;
+
+        bytes memory bitmask = abi.encodePacked(
             bytes32(uint256(type(uint160).max)),
             bytes32(uint256(type(uint160).max)),
             bytes32(type(uint256).max),
             new bytes(callData.length)
         );
+        console.logBytes32(verifier.calculateHash(bitmask, who, where, value, callData));
+        console.logBytes(bitmask);
+        console.logBytes(callData);
+        console.log(callData.length, bitmask.length);
 
         bytes memory verificationData = abi.encode(
             verifier.calculateHash(bitmask, who, where, value, callData),
             bitmask // bitmask
         );
 
-        verifier.verifyCall(who, where, value, callData, verificationData);
+        assertTrue(verifier.verifyCall(who, where, value, callData, verificationData));
+    }
+
+    function testInvalid() external {
+        BitmaskVerifier verifier = new BitmaskVerifier();
+        uint256 value = 100 ether;
+
+        bytes memory bitmask = abi.encodePacked(
+            bytes32(uint256(type(uint160).max)),
+            bytes32(uint256(type(uint160).max)),
+            bytes32(type(uint256).max),
+            new bytes(callData.lewngth)
+        );
+
+        bytes memory verificationData =
+            abi.encode(keccak256("verifier.calculateHash(bitmask, who, where, value, callData)"), bitmask);
+
+        assertFalse(verifier.verifyCall(who, where, value, callData, verificationData));
+
+        assertFalse(verifier.verifyCall(who, where, value, wrongCallData, verificationData));
     }
 }
