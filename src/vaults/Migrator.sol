d66a71f802b22fe017c56a7dedd7e9a3e279b8d4
Andrey Borzenkov <armoking32@gmail.com>
2025-10-07 17:18:07 +0400

    fix

--- src/vaults/Migrator.sol
+++ src/vaults/Migrator.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.25;
 
+import "./Vault.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
@@ -33,10 +34,10 @@ contract Migrator is Ownable {
     address public constant MULTICALL = 0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441;
     address public constant REDIRECTING_DEPOSIT_HOOK = 0x00000004d3B17e5391eb571dDb8fDF95646ca827;
     address public constant BASIC_REDEEM_HOOK = 0x0000000637f1b1ccDA4Af2dB6CDDf5e5Ec45fd93;
-    IFactory public constant FEE_MANAGER_FACTORY = 0xF7223356819Ea48f25880b6c2ab3e907CC336D45;
-    IFactory public constant RISK_MANAGER_FACTORY = 0xa51E4FA916b939Fa451520D2B7600c740d86E5A0;
-    IFactory public constant VAULT_FACTORY = 0x4E38F679e46B3216f0bd4B314E9C429AFfB1dEE3;
-    IFactory public constant ORACLE_FACTORY = 0x0CdFf250C7a071fdc72340D820C5C8e29507Aaad;
+    IFactory public constant FEE_MANAGER_FACTORY = IFactory(0xF7223356819Ea48f25880b6c2ab3e907CC336D45);
+    IFactory public constant RISK_MANAGER_FACTORY = IFactory(0xa51E4FA916b939Fa451520D2B7600c740d86E5A0);
+    IFactory public constant VAULT_FACTORY = IFactory(0x4E38F679e46B3216f0bd4B314E9C429AFfB1dEE3);
+    IFactory public constant ORACLE_FACTORY = IFactory(0x0CdFf250C7a071fdc72340D820C5C8e29507Aaad);
     address public constant TOKENIZED_SHARE_MANAGER = 0x0000000E8eb7173fA1a3ba60eCA325bcB6aaf378;
 
     constructor(address owner_) Ownable(owner_) {}
@@ -50,7 +51,7 @@ contract Migrator is Ownable {
             address oracle = ORACLE_FACTORY.create(0, params.proxyAdminOwner, params.oracleParams);
             bytes memory initParams = abi.encode(
                 params.vaultAdmin,
-                vault,
+                multiVault,
                 feeManager,
                 riskManager,
                 oracle,
@@ -76,11 +77,10 @@ contract Migrator is Ownable {
         );
 
         IMulticall.Call[] memory calls = new IMulticall.Call[](2);
-        calls[0] =
-            IMulticall.Call({target: asset, callData: abi.encodeCall(IERC20.transfer, (multiVault, assetBalance))});
+        calls[0] = IMulticall.Call({target: asset, callData: abi.encodeCall(IERC20.transfer, (vault, assetBalance))});
         calls[1] = IMulticall.Call({
             target: defaultCollateral,
-            callData: abi.encodeCall(IERC20.transfer, (multiVault, defaultCollateralBalance))
+            callData: abi.encodeCall(IERC20.transfer, (vault, defaultCollateralBalance))
         });
         params.proxyAdmin.upgradeAndCall(
             ITransparentUpgradeableProxy(multiVault), MULTICALL, abi.encodeCall(IMulticall.aggregate, calls)
@@ -90,5 +90,7 @@ contract Migrator is Ownable {
             ITransparentUpgradeableProxy(multiVault), TOKENIZED_SHARE_MANAGER, new bytes(0)
         );
         IShareManager(multiVault).setVault(vault);
+
+        params.proxyAdmin.transferOwnership(params.proxyAdminOwner);
     }
 }
