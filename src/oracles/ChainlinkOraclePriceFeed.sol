cc68b11de97e6f3fcec3b478c2951f70e06427d4
Andrey Borzenkov <armoking32@gmail.com>
2025-10-18 13:34:01 +0400

    price feed

--- src/oracles/ChainlinkOraclePriceFeed.sol
+++ src/oracles/ChainlinkOraclePriceFeed.sol
@@ -2,6 +2,11 @@
 pragma solidity 0.8.25;
 
 import "../interfaces/oracles/IOracle.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+interface IWSTETH {
+    function getStETHByWstETH(uint256 amount) external view returns (uint256);
+}
 
 contract ChainlinkOraclePriceFeed {
     error ZeroPrice();
@@ -9,11 +14,10 @@ contract ChainlinkOraclePriceFeed {
 
     address public immutable oracle;
     address public immutable baseAsset;
-    address public immutable wstethToEth;
-    address public immutable revertOnSuspicious;
+    bool public immutable wstethToEth;
+    bool public immutable revertOnSuspicious;
 
     uint8 public constant decimals = 18;
-    uint256 public constant latestRound = 0;
 
     constructor(address oracle_, address baseAsset_, bool wstethToEth_, bool revertOnSuspicious_) {
         oracle = oracle_;
@@ -25,7 +29,7 @@ contract ChainlinkOraclePriceFeed {
     function latestRoundData()
         public
         view
-        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+        returns (uint80, int256 answer, uint256 startedAt, uint256 updatedAt, uint80)
     {
         IOracle.DetailedReport memory report = IOracle(oracle).getReport(baseAsset);
         if (report.priceD18 == 0) {
@@ -36,18 +40,18 @@ contract ChainlinkOraclePriceFeed {
             revert SuspiciousReport();
         }
 
-        roundId = 0;
-        answeredInRound = 0;
-        answer = 1e36 / report.priceD18;
-        updatedAt = report.timestamp;
+        startedAt = report.timestamp;
+        updatedAt = updatedAt;
+
+        uint256 priceD18 = 1e36 / report.priceD18;
+        if (wstethToEth) {
+            priceD18 = IWSTETH(baseAsset).getStETHByWstETH(priceD18);
+        }
+        answer = int256(priceD18);
     }
 
-    function latestAnswer() external view returns (int256 answer) {
+    function latestAnswer() public view returns (int256 answer) {
         (, answer,,,) = latestRoundData();
-    }
-
-    function latestTimestamp() external view returns (uint256 timestamp) {
-        (,,, timestamp,) = latestRoundData();
     }
 
     function getRate() public view returns (uint256) {
