9ab4765c7be524ff97660e2f8acda0a53349688f
Anton Iants <yants.anton@gmail.com>
2025-10-21 15:38:19 +0400

    base test vault extension with agent setup

--- scripts/base/tqETH.s.sol
+++ scripts/base/tqETH.s.sol
@@ -9,6 +9,7 @@ import "../collectors/defi/external/IAaveOracleV3.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/governance/TimelockController.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 import "../../src/vaults/Subvault.sol";
 import "../../src/vaults/VaultConfigurator.sol";
@@ -31,6 +32,9 @@ contract Deploy is Script {
     address public pauser = address(0x5Ee08C98FaD00C492Deba19e9d038fEFd275E8C0);
 
     function run() external {
+        _addAgentManagedSubvaults(curator);
+        return;
+
         uint256 deployerPk = uint256(bytes32(vm.envBytes("HOT_DEPLOYER")));
         address deployer = vm.addr(deployerPk);
 
@@ -332,5 +336,190 @@ contract Deploy is Script {
         ProofLibrary.storeProofs("base:tqETH:subvault0", merkleRoot, leaves, descriptions);
         calls = tqETHLibrary.getSubvault0SubvaultCalls($, curator, leaves);
         verifier = $.verifierFactory.create(0, proxyAdmin, abi.encode(vault, merkleRoot));
+    }
+
+    function _deployAgentVerifiers(address agent) internal {
+        address vault = 0xD014de79cCC9b09376261F106773824F91748a68;
+
+        ProtocolDeployment memory $ = Constants.protocolDeployment();
+        uint256 deployerPk = uint256(bytes32(vm.envBytes("HOT_DEPLOYER")));
+        address deployer = vm.addr(deployerPk);
+        console2.log("Deployer %s", deployer);
+        vm.startBroadcast(deployerPk);
+        for (uint256 i = 1; i < 3; i++) {
+            string[] memory descriptions = tqETHLibraryV2.getSubvault0Descriptions(curator, agent);
+            (bytes32 merkleRoot, IVerifier.VerificationPayload[] memory leaves) =
+                tqETHLibraryV2.getSubvault0Proofs(curator, agent);
+            ProofLibrary.storeProofs(
+                string(abi.encodePacked("base:tqETH:subvault", Strings.toString(i))), merkleRoot, leaves, descriptions
+            );
+
+            address verifier = $.verifierFactory.create(0, proxyAdmin, abi.encode(vault, merkleRoot));
+            console2.log("Verifier %s %s", i, verifier);
+        }
+        vm.stopBroadcast();
+        revert("ok");
+    }
+
+    function _addAgentManagedSubvaults(address agent) internal {
+        Vault vault = Vault(payable(0xD014de79cCC9b09376261F106773824F91748a68));
+        address timelockController = 0xb4EDFD431aB522223c5499F1b2E7493fb211eF84;
+        address deployer = 0xE98Be1E5538FCbD716C506052eB1Fd5d6fC495A3;
+
+        vm.startPrank(lazyVaultAdmin);
+        vault.grantRole(Permissions.CREATE_SUBVAULT_ROLE, lazyVaultAdmin);
+        vault.grantRole(Permissions.SET_HOOK_ROLE, lazyVaultAdmin);
+
+        vault.grantRole(Permissions.CALLER_ROLE, agent);
+        vault.grantRole(Permissions.PULL_LIQUIDITY_ROLE, agent);
+        vault.grantRole(Permissions.PUSH_LIQUIDITY_ROLE, agent);
+
+        address[] memory verifiers = new address[](3);
+        verifiers[0] = 0xB6f2b30244e32e7e95Ffa1A23797131aAb896410;
+        verifiers[1] = 0x178d24C71437aea2739A6C9E28162793c40A919c;
+        verifiers[2] = 0x3e84A470cfD72C6153da3759FB0c22FF05B466e7;
+        SubvaultCalls[] memory calls = new SubvaultCalls[](3);
+        (, calls[0]) = _createCowswapVerifier(address(vault));
+
+        (address[] memory assets_, VaultConfigurator.InitParams memory initParams) =
+            _getInitParams(deployer, timelockController);
+        ProtocolDeployment memory $ = Constants.protocolDeployment();
+        {
+            for (uint256 i = 1; i < 3; i++) {
+                string[] memory descriptions = tqETHLibraryV2.getSubvault0Descriptions(curator, agent);
+                (bytes32 merkleRoot, IVerifier.VerificationPayload[] memory leaves) =
+                    tqETHLibraryV2.getSubvault0Proofs(curator, agent);
+                ProofLibrary.storeProofs(
+                    string(abi.encodePacked("base:tqETH:subvault", Strings.toString(i))),
+                    merkleRoot,
+                    leaves,
+                    descriptions
+                );
+
+                calls[i] = tqETHLibraryV2.getSubvault0SubvaultCalls($, curator, agent, leaves);
+                address subvault = vault.createSubvault(0, proxyAdmin, verifiers[i]); // eth,weth,wsteth
+                IRiskManager riskManager = vault.riskManager();
+                riskManager.allowSubvaultAssets(
+                    subvault,
+                    ArraysLibrary.makeAddressArray(abi.encode(Constants.ETH, Constants.WETH, Constants.WSTETH))
+                );
+                riskManager.setSubvaultLimit(subvault, type(int256).max / 2);
+            }
+        }
+
+        for (uint256 i = 0; i < vault.subvaults() + 2; i++) {
+            vault.setDefaultDepositHook(address(0));
+        }
+        for (uint256 index = 0; index < assets_.length; index++) {
+            for (uint256 i = 0; i < vault.getQueueCount(assets_[index]); i++) {
+                address queue = vault.queueAt(assets_[index], i);
+                if (vault.isDepositQueue(queue)) {
+                    vault.setDefaultDepositHook(address(0));
+                    vault.setCustomHook(queue, address(0));
+                }
+            }
+        }
+        vault.renounceRole(Permissions.CREATE_SUBVAULT_ROLE, lazyVaultAdmin);
+        vault.renounceRole(Permissions.SET_HOOK_ROLE, lazyVaultAdmin);
+        vm.stopPrank();
+
+        AcceptanceLibrary.runProtocolDeploymentChecks($);
+        AcceptanceLibrary.runVaultDeploymentChecks(
+            $,
+            VaultDeployment({
+                vault: vault,
+                calls: calls,
+                initParams: initParams,
+                holders: _getExpectedHolders(timelockController),
+                depositHook: address(0),
+                redeemHook: address($.basicRedeemHook),
+                assets: assets_,
+                depositQueueAssets: assets_,
+                redeemQueueAssets: assets_,
+                subvaultVerifiers: verifiers,
+                timelockControllers: ArraysLibrary.makeAddressArray(abi.encode(timelockController)),
+                timelockProposers: ArraysLibrary.makeAddressArray(abi.encode(lazyVaultAdmin, deployer)),
+                timelockExecutors: ArraysLibrary.makeAddressArray(abi.encode(lazyVaultAdmin))
+            })
+        );
+    }
+
+    function _getInitParams(address deployer, address timelockController)
+        internal
+        view
+        returns (address[] memory assets_, VaultConfigurator.InitParams memory initParams)
+    {
+        Vault.RoleHolder[] memory holders = new Vault.RoleHolder[](42);
+        {
+            uint256 i = 0;
+
+            // activeVaultAdmin roles:
+            holders[i++] = Vault.RoleHolder(Permissions.ACCEPT_REPORT_ROLE, activeVaultAdmin);
+            holders[i++] = Vault.RoleHolder(Permissions.SET_MERKLE_ROOT_ROLE, activeVaultAdmin);
+            holders[i++] = Vault.RoleHolder(Permissions.ALLOW_CALL_ROLE, activeVaultAdmin);
+            holders[i++] = Vault.RoleHolder(Permissions.DISALLOW_CALL_ROLE, activeVaultAdmin);
+            holders[i++] = Vault.RoleHolder(Permissions.SET_VAULT_LIMIT_ROLE, activeVaultAdmin);
+            holders[i++] = Vault.RoleHolder(Permissions.SET_SUBVAULT_LIMIT_ROLE, activeVaultAdmin);
+            holders[i++] = Vault.RoleHolder(Permissions.ALLOW_SUBVAULT_ASSETS_ROLE, activeVaultAdmin);
+            holders[i++] = Vault.RoleHolder(Permissions.MODIFY_VAULT_BALANCE_ROLE, activeVaultAdmin);
+            holders[i++] = Vault.RoleHolder(Permissions.MODIFY_SUBVAULT_BALANCE_ROLE, activeVaultAdmin);
+
+            // emergeny pauser roles:
+            holders[i++] = Vault.RoleHolder(Permissions.SET_FLAGS_ROLE, address(timelockController));
+            holders[i++] = Vault.RoleHolder(Permissions.SET_MERKLE_ROOT_ROLE, address(timelockController));
+            holders[i++] = Vault.RoleHolder(Permissions.SET_QUEUE_STATUS_ROLE, address(timelockController));
+
+            // oracle updater roles:
+            holders[i++] = Vault.RoleHolder(Permissions.SUBMIT_REPORTS_ROLE, oracleUpdater);
+
+            // curator roles:
+            holders[i++] = Vault.RoleHolder(Permissions.CALLER_ROLE, curator);
+            holders[i++] = Vault.RoleHolder(Permissions.PULL_LIQUIDITY_ROLE, curator);
+            holders[i++] = Vault.RoleHolder(Permissions.PUSH_LIQUIDITY_ROLE, curator);
+
+            // deployer roles:
+            holders[i++] = Vault.RoleHolder(Permissions.CREATE_QUEUE_ROLE, deployer);
+            holders[i++] = Vault.RoleHolder(Permissions.CREATE_SUBVAULT_ROLE, deployer);
+            holders[i++] = Vault.RoleHolder(Permissions.SET_VAULT_LIMIT_ROLE, deployer);
+            holders[i++] = Vault.RoleHolder(Permissions.ALLOW_SUBVAULT_ASSETS_ROLE, deployer);
+            holders[i++] = Vault.RoleHolder(Permissions.SET_SUBVAULT_LIMIT_ROLE, deployer);
+            holders[i++] = Vault.RoleHolder(Permissions.SUBMIT_REPORTS_ROLE, deployer);
+            holders[i++] = Vault.RoleHolder(Permissions.ACCEPT_REPORT_ROLE, deployer);
+            assembly {
+                mstore(holders, i)
+            }
+        }
+
+        assets_ = ArraysLibrary.makeAddressArray(abi.encode(Constants.ETH, Constants.WETH, Constants.WSTETH));
+
+        ProtocolDeployment memory $ = Constants.protocolDeployment();
+        initParams = VaultConfigurator.InitParams({
+            version: 0,
+            proxyAdmin: proxyAdmin,
+            vaultAdmin: lazyVaultAdmin,
+            shareManagerVersion: 0,
+            shareManagerParams: abi.encode(bytes32(0), "Theoriq AlphaVault ETH", "tqETH"),
+            feeManagerVersion: 0,
+            feeManagerParams: abi.encode(deployer, lazyVaultAdmin, uint24(0), uint24(0), uint24(1e5), uint24(1e4)),
+            riskManagerVersion: 0,
+            riskManagerParams: abi.encode(type(int256).max / 2),
+            oracleVersion: 0,
+            oracleParams: abi.encode(
+                IOracle.SecurityParams({
+                    maxAbsoluteDeviation: 0.005 ether,
+                    suspiciousAbsoluteDeviation: 0.001 ether,
+                    maxRelativeDeviationD18: 0.005 ether,
+                    suspiciousRelativeDeviationD18: 0.001 ether,
+                    timeout: 20 hours,
+                    depositInterval: 1 hours,
+                    redeemInterval: 2 days
+                }),
+                assets_
+            ),
+            defaultDepositHook: address($.redirectingDepositHook),
+            defaultRedeemHook: address($.basicRedeemHook),
+            queueLimit: 6,
+            roleHolders: holders
+        });
     }
 }
--- scripts/base/tqETHLibrary.sol
+++ scripts/base/tqETHLibrary.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.25;
 import {ArraysLibrary} from "../common/ArraysLibrary.sol";
 import {ProofLibrary} from "../common/ProofLibrary.sol";
 
-import {CowSwapLibrary} from "../common/CowSwapLibrary.sol";
-import {WethLibrary} from "../common/WethLibrary.sol";
+import {CowSwapLibrary} from "../common/protocols/CowSwapLibrary.sol";
+import {WethLibrary} from "../common/protocols/WethLibrary.sol";
 
 import {Call, IVerifier, ProtocolDeployment, SubvaultCalls} from "../common/interfaces/Imports.sol";
 
@@ -84,6 +84,135 @@ library tqETHLibrary {
                 })
             ),
             2
+        );
+    }
+}
+
+library tqETHLibraryV2 {
+    function getSubvault0Proofs(address curator, address agent)
+        internal
+        pure
+        returns (bytes32 merkleRoot, IVerifier.VerificationPayload[] memory leaves)
+    {
+        ProtocolDeployment memory $ = Constants.protocolDeployment();
+        /*
+            1. weth.deposit{value: <any>}();
+            2. weth.withdraw(<any>);
+            3. weth.approve(cowswapVaultRelayer, <any>);
+            4. wsteth.approve(cowswapVaultRelayer, <any>);
+            5. cowswapSettlement.setPreSignature(anyBytes(56), anyBool);
+            6. cowswapSettlement.invalidateOrder(anyBytes(56));
+        */
+        leaves = new IVerifier.VerificationPayload[](12);
+        leaves[0] = WethLibrary.getWethDepositProof($.bitmaskVerifier, WethLibrary.Info(curator, Constants.WETH));
+        leaves[1] = WethLibrary.getWethWithdrawProof($.bitmaskVerifier, WethLibrary.Info(curator, Constants.WETH));
+
+        leaves[2] = WethLibrary.getWethDepositProof($.bitmaskVerifier, WethLibrary.Info(agent, Constants.WETH));
+        leaves[3] = WethLibrary.getWethWithdrawProof($.bitmaskVerifier, WethLibrary.Info(agent, Constants.WETH));
+
+        ArraysLibrary.insert(
+            leaves,
+            CowSwapLibrary.getCowSwapProofs(
+                $.bitmaskVerifier,
+                CowSwapLibrary.Info({
+                    cowswapSettlement: Constants.COWSWAP_SETTLEMENT,
+                    cowswapVaultRelayer: Constants.COWSWAP_VAULT_RELAYER,
+                    curator: curator,
+                    assets: ArraysLibrary.makeAddressArray(abi.encode(Constants.WETH, Constants.WSTETH))
+                })
+            ),
+            4
+        );
+        ArraysLibrary.insert(
+            leaves,
+            CowSwapLibrary.getCowSwapProofs(
+                $.bitmaskVerifier,
+                CowSwapLibrary.Info({
+                    cowswapSettlement: Constants.COWSWAP_SETTLEMENT,
+                    cowswapVaultRelayer: Constants.COWSWAP_VAULT_RELAYER,
+                    curator: agent,
+                    assets: ArraysLibrary.makeAddressArray(abi.encode(Constants.WETH, Constants.WSTETH))
+                })
+            ),
+            8
+        );
+        return ProofLibrary.generateMerkleProofs(leaves);
+    }
+
+    function getSubvault0Descriptions(address curator, address agent)
+        internal
+        view
+        returns (string[] memory descriptions)
+    {
+        descriptions = new string[](12);
+        descriptions[0] = WethLibrary.getWethDepositDescription(WethLibrary.Info(curator, Constants.WETH));
+        descriptions[1] = WethLibrary.getWethWithdrawDescription(WethLibrary.Info(curator, Constants.WETH));
+
+        descriptions[2] = WethLibrary.getWethDepositDescription(WethLibrary.Info(agent, Constants.WETH));
+        descriptions[3] = WethLibrary.getWethWithdrawDescription(WethLibrary.Info(agent, Constants.WETH));
+        ArraysLibrary.insert(
+            descriptions,
+            CowSwapLibrary.getCowSwapDescriptions(
+                CowSwapLibrary.Info({
+                    cowswapSettlement: Constants.COWSWAP_SETTLEMENT,
+                    cowswapVaultRelayer: Constants.COWSWAP_VAULT_RELAYER,
+                    curator: curator,
+                    assets: ArraysLibrary.makeAddressArray(abi.encode(Constants.WETH, Constants.WSTETH))
+                })
+            ),
+            4
+        );
+        ArraysLibrary.insert(
+            descriptions,
+            CowSwapLibrary.getCowSwapDescriptions(
+                CowSwapLibrary.Info({
+                    cowswapSettlement: Constants.COWSWAP_SETTLEMENT,
+                    cowswapVaultRelayer: Constants.COWSWAP_VAULT_RELAYER,
+                    curator: agent,
+                    assets: ArraysLibrary.makeAddressArray(abi.encode(Constants.WETH, Constants.WSTETH))
+                })
+            ),
+            8
+        );
+    }
+
+    function getSubvault0SubvaultCalls(
+        ProtocolDeployment memory $,
+        address curator,
+        address agent,
+        IVerifier.VerificationPayload[] memory leaves
+    ) internal pure returns (SubvaultCalls memory calls) {
+        calls.payloads = leaves;
+        calls.calls = new Call[][](leaves.length);
+
+        calls.calls[0] = WethLibrary.getWethDepositCalls(WethLibrary.Info(curator, Constants.WETH));
+        calls.calls[1] = WethLibrary.getWethWithdrawCalls(WethLibrary.Info(curator, Constants.WETH));
+
+        calls.calls[2] = WethLibrary.getWethDepositCalls(WethLibrary.Info(agent, Constants.WETH));
+        calls.calls[3] = WethLibrary.getWethWithdrawCalls(WethLibrary.Info(agent, Constants.WETH));
+        ArraysLibrary.insert(
+            calls.calls,
+            CowSwapLibrary.getCowSwapCalls(
+                CowSwapLibrary.Info({
+                    cowswapSettlement: Constants.COWSWAP_SETTLEMENT,
+                    cowswapVaultRelayer: Constants.COWSWAP_VAULT_RELAYER,
+                    curator: curator,
+                    assets: ArraysLibrary.makeAddressArray(abi.encode(Constants.WETH, Constants.WSTETH))
+                })
+            ),
+            4
+        );
+        ArraysLibrary.insert(
+            calls.calls,
+            CowSwapLibrary.getCowSwapCalls(
+                CowSwapLibrary.Info({
+                    cowswapSettlement: Constants.COWSWAP_SETTLEMENT,
+                    cowswapVaultRelayer: Constants.COWSWAP_VAULT_RELAYER,
+                    curator: agent,
+                    assets: ArraysLibrary.makeAddressArray(abi.encode(Constants.WETH, Constants.WSTETH))
+                })
+            ),
+            8
         );
     }
 }
