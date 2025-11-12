cbe2772c6377184a518b8dfc437f85596574c4da
Anton Iants <yants.anton@gmail.com>
2025-11-12 15:33:36 +0400

    add comments

--- scripts/ethereum/mAlphaLibrary.sol
+++ scripts/ethereum/mAlphaLibrary.sol
@@ -35,19 +35,29 @@ library mAlphaLibrary2 {
         address termmaxMarket;
         address morphoStrategyWrapper;
     }
-
+    /**
+        subvault 0:
+            - DigiFTI: uMINT <-> USDC
+            - TermMax: borrow/repay USDU using uMINT as collateral
+            - Curve swap: USDU <> USDC
+        subvault 1:
+            - Morpho: supply/withdraw USDC
+            - MorphoStrategyWrapper: borrow/repay USDC using USDU as collateral
+            - rewardVault 4626 interaction
+            - Curve mint/burn liquidity USDU / USDC 
+     */
     function getSubvault0Proofs(Info memory $)
         internal
         view
         returns (bytes32 merkleRoot, IVerifier.VerificationPayload[] memory leaves)
     {
         /*
-             0. IERC20(USDC).approve(SUB_RED_MANAGEMENT, ...)
-             1. IERC20(uMINT).approve(SUB_RED_MANAGEMENT, ...)
-             2. IERC20(uMINT).approve(TERMMAX_ROUTER, ...)
-             3. IERC20(USDU).approve(TERMMAX_ROUTER, ...)
-             4. IERC20(USDU).approve(CURVE_POOL, ...)
-             5. IERC20(USDC).approve(CURVE_POOL, ...)
+             0. IERC20(USDC).approve(SUB_RED_MANAGEMENT, ...) to buy uMINT for USDC
+             1. IERC20(uMINT).approve(SUB_RED_MANAGEMENT, ...) to sell uMINT for USDC
+             2. IERC20(uMINT).approve(TERMMAX_ROUTER, ...) to supply uMINT as collateral
+             3. IERC20(USDU).approve(TERMMAX_ROUTER, ...) to repay USDU
+             4. IERC20(USDU).approve(CURVE_POOL, ...) to swap USDU <> USDC
+             5. IERC20(USDC).approve(CURVE_POOL, ...) to swap USDC <> USDU
              6. SUB_RED_MANAGEMENT.subscribe(UMINT, USDC, ...)
              7. SUB_RED_MANAGEMENT.redeem(UMINT, USDC, ...)
              8. TERMMAX_ROUTER.borrowTokenFromCollateral(subvault0, MARKET, ...) (uMINT->USDU)
