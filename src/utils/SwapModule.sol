49436a4c049c255b939b12f67e38c63a2a3b15d8
Andrey Borzenkov <armoking32@gmail.com>
2025-11-13 19:36:32 +0900

    multipliers & oracles

--- src/utils/SwapModule.sol
+++ src/utils/SwapModule.sol
@@ -1,48 +1,51 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.25;
 
-import "../interfaces/external/cowswap/ICowswapSettlement.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../interfaces/utils/ISwapModule.sol";
 
 import "../libraries/TransferLibrary.sol";
-import "../libraries/external/GPv2Order.sol";
-import "../oracles/ConversionOracle.sol";
 import "../permissions/MellowACL.sol";
 
-contract SwapModule is MellowACL {
+contract SwapModule is ISwapModule, MellowACL {
     using SafeERC20 for IERC20;
 
-    error Forbidden(string reason);
-    error ZeroValue();
+    /// @inheritdoc ISwapModule
+    uint256 public constant MIN_MULTIPLIER = 0.9e8;
+    /// @inheritdoc ISwapModule
+    uint256 public constant MAX_MULTIPLIER = 1.1e8;
+    /// @inheritdoc ISwapModule
+    uint256 public constant BASE_MULTIPLIER = 1e8;
 
-    struct SwapModuleStorage {
-        address subvault;
-        address oracle;
-    }
-
-    struct Params {
-        address tokenIn;
-        address tokenOut;
-        uint256 amountIn;
-        uint256 minAmountOut;
-        uint256 deadline;
-    }
-
+    /// @inheritdoc ISwapModule
     bytes32 public constant TOKEN_IN_ROLE = keccak256("utils.SwapModule.TOKEN_IN_ROLE");
+    /// @inheritdoc ISwapModule
     bytes32 public constant TOKEN_OUT_ROLE = keccak256("utils.SwapModule.TOKEN_OUT_ROLE");
+    /// @inheritdoc ISwapModule
     bytes32 public constant ROUTER_ROLE = keccak256("utils.SwapModule.ROUTER_ROLE");
+    /// @inheritdoc ISwapModule
     bytes32 public constant CALLER_ROLE = keccak256("utils.SwapModule.CALLER_ROLE");
+    /// @inheritdoc ISwapModule
+    bytes32 public constant SET_SLIPPAGE_ROLE = keccak256("utils.SwapModule.SET_SLIPPAGE_ROLE");
 
+    /// @inheritdoc ISwapModule
     address public immutable cowswapSettlement;
+    /// @inheritdoc ISwapModule
     address public immutable cowswapVaultRelayer;
+    /// @inheritdoc ISwapModule
+    address public immutable weth;
 
     bytes32 private immutable _swapModuleStorageSlot;
 
-    constructor(string memory name_, uint256 version_, address cowswapSettlement_, address cowswapVaultRelayer_)
-        MellowACL(name_, version_)
-    {
+    constructor(
+        string memory name_,
+        uint256 version_,
+        address cowswapSettlement_,
+        address cowswapVaultRelayer_,
+        address weth_
+    ) MellowACL(name_, version_) {
         cowswapSettlement = cowswapSettlement_;
         cowswapVaultRelayer = cowswapVaultRelayer_;
+        weth = weth_;
         _swapModuleStorageSlot = SlotLibrary.getSlot("SwapModule", name_, version_);
     }
 
@@ -55,14 +58,44 @@ contract SwapModule is MellowACL {
         _;
     }
 
+    /// @inheritdoc ISwapModule
     function subvault() public view returns (address) {
         return _swapModuleStorage().subvault;
     }
 
+    /// @inheritdoc ISwapModule
     function oracle() public view returns (address) {
         return _swapModuleStorage().oracle;
     }
 
+    /// @inheritdoc ISwapModule
+    function checkMultiplier(uint256 multiplier) public pure {
+        if (multiplier < MIN_MULTIPLIER || multiplier > MAX_MULTIPLIER) {
+            revert Forbidden("multiplier out of expected range");
+        }
+    }
+
+    /// @inheritdoc ISwapModule
+    function defaultMultiplier() public view returns (uint256) {
+        return _swapModuleStorage().defaultMultiplier;
+    }
+
+    /// @inheritdoc ISwapModule
+    function customMultiplier(address tokenIn, address tokenOut) public view returns (uint256) {
+        return _swapModuleStorage().customMultiplier[tokenIn][tokenOut];
+    }
+
+    /// @inheritdoc ISwapModule
+    function evaluate(address tokenIn, address tokenOut, uint256 amountIn) public view returns (uint256) {
+        IAaveOracle oracle_ = IAaveOracle(oracle());
+        return Math.mulDiv(
+            amountIn,
+            oracle_.getAssetPrice(tokenIn == TransferLibrary.ETH ? weth : tokenIn),
+            oracle_.getAssetPrice(tokenOut == TransferLibrary.ETH ? weth : tokenOut)
+        );
+    }
+
+    /// @inheritdoc ISwapModule
     function checkParams(Params calldata params) public view {
         if (!hasRole(TOKEN_IN_ROLE, params.tokenIn)) {
             revert Forbidden("tokenIn");
@@ -73,7 +106,14 @@ contract SwapModule is MellowACL {
         if (TransferLibrary.balanceOf(params.tokenIn, address(this)) < params.amountIn || params.amountIn == 0) {
             revert Forbidden("amountIn");
         }
-        uint256 oracleMinAmount = ConversionOracle(oracle()).evaluate(params.tokenIn, params.tokenOut, params.amountIn);
+        SwapModuleStorage storage $ = _swapModuleStorage();
+        uint256 multiplier = $.customMultiplier[params.tokenIn][params.tokenOut];
+        if (multiplier == 0) {
+            multiplier = $.defaultMultiplier;
+        }
+        uint256 oracleMinAmount =
+            Math.mulDiv(evaluate(params.tokenIn, params.tokenOut, params.amountIn), multiplier, BASE_MULTIPLIER);
+
         if (params.minAmountOut < oracleMinAmount) {
             revert Forbidden("minAmountOut < oracleMinAmount");
         }
@@ -85,6 +125,7 @@ contract SwapModule is MellowACL {
         }
     }
 
+    /// @inheritdoc ISwapModule
     function checkCowswapOrder(Params calldata params, GPv2Order.Data calldata order, bytes calldata orderUid)
         public
         view
@@ -131,32 +172,72 @@ contract SwapModule is MellowACL {
 
     // Mutable functions
 
+    /// @inheritdoc IFactoryEntity
     function initialize(bytes calldata data) external initializer {
-        (address admin, address subvault_, address oracle_, address[] memory holders, bytes32[] memory roles) =
-            abi.decode(data, (address, address, address, address[], bytes32[]));
+        (
+            address admin,
+            address subvault_,
+            address oracle_,
+            uint256 defaultMultiplier_,
+            address[] memory holders,
+            bytes32[] memory roles
+        ) = abi.decode(data, (address, address, address, uint256, address[], bytes32[]));
         if (admin == address(0) || subvault_ == address(0) || oracle_ == address(0)) {
             revert ZeroValue();
         }
+        checkMultiplier(defaultMultiplier_);
+        SwapModuleStorage storage $ = _swapModuleStorage();
+
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
-        _swapModuleStorage().subvault = subvault_;
-        _swapModuleStorage().oracle = oracle_;
+        $.subvault = subvault_;
+        $.oracle = oracle_;
+        $.defaultMultiplier = defaultMultiplier_;
         for (uint256 i = 0; i < holders.length; i++) {
             if (holders[i] == address(0) || roles[i] == bytes32(0)) {
                 revert ZeroValue();
             }
             _grantRole(roles[i], holders[i]);
         }
+        emit Initialized(data);
     }
 
+    /// @inheritdoc ISwapModule
+    function setOracle(address oracle_) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (oracle_ == address(0)) {
+            revert ZeroValue();
+        }
+        _swapModuleStorage().oracle = oracle_;
+        emit OracleSet(oracle_);
+    }
+
+    /// @inheritdoc ISwapModule
+    function setDefaultMultiplier(uint256 multiplier) external onlyRole(SET_SLIPPAGE_ROLE) {
+        checkMultiplier(multiplier);
+        _swapModuleStorage().defaultMultiplier = multiplier;
+        emit DefaultMultiplierSet(multiplier);
+    }
+
+    /// @inheritdoc ISwapModule
+    function setCustomMultiplier(address tokenIn, address tokenOut, uint256 multiplier)
+        external
+        onlyRole(SET_SLIPPAGE_ROLE)
+    {
+        checkMultiplier(multiplier);
+        _swapModuleStorage().customMultiplier[tokenIn][tokenOut] = multiplier;
+        emit CustomMultiplierSet(tokenIn, tokenOut, multiplier);
+    }
+
+    /// @inheritdoc ISwapModule
     function pushAssets(address asset, uint256 amount) external payable onlySubvault {
         TransferLibrary.receiveAssets(asset, _msgSender(), amount);
     }
 
+    /// @inheritdoc ISwapModule
     function pullAssets(address asset, uint256 amount) external onlySubvault {
         TransferLibrary.sendAssets(asset, _msgSender(), amount);
     }
 
-    // onchain swap via whitelisted router
+    /// @inheritdoc ISwapModule
     function swap(Params calldata params, address router, bytes calldata data)
         external
         onlyRole(CALLER_ROLE)
@@ -178,15 +259,19 @@ contract SwapModule is MellowACL {
         if (amountOut < params.minAmountOut) {
             revert Forbidden("amountOut < minAmountOut");
         }
+        emit Swap(params, router, amountOut);
     }
 
+    /// @inheritdoc ISwapModule
     function setCowswapApproval(address asset, uint256 amount) external onlyRole(CALLER_ROLE) {
         if (!hasRole(TOKEN_IN_ROLE, asset)) {
             revert Forbidden("asset");
         }
         IERC20(asset).forceApprove(cowswapVaultRelayer, amount);
+        emit CowswapApprovalSet(asset, amount);
     }
 
+    /// @inheritdoc ISwapModule
     function createLimitOrder(Params calldata params, GPv2Order.Data calldata order, bytes calldata orderUid)
         external
         onlyRole(CALLER_ROLE)
@@ -194,10 +279,13 @@ contract SwapModule is MellowACL {
         checkParams(params);
         checkCowswapOrder(params, order, orderUid);
         ICowswapSettlement(cowswapSettlement).setPreSignature(orderUid, true);
+        emit LimitOrderCreated(params, orderUid);
     }
 
-    function cancelLimitOrder(bytes calldata orderUid) external onlyRole(CALLER_ROLE) {
+    /// @inheritdoc ISwapModule
+    function invalidateOrder(bytes calldata orderUid) external onlyRole(CALLER_ROLE) {
         ICowswapSettlement(cowswapSettlement).invalidateOrder(orderUid);
+        emit LimitOrderInvalidated(orderUid);
     }
 
     receive() external payable {}
