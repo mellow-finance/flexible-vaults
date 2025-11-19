520bc310c4d7f3769e42943cbad966aa8613b69b
Anton Iants <yants.anton@gmail.com>
2025-11-19 15:45:39 +0400

    update deploy collector script

--- scripts/monad/DeployCollector.s.sol
+++ scripts/monad/DeployCollector.s.sol
@@ -2,85 +2,90 @@
 pragma solidity 0.8.25;
 
 import "../../scripts/collectors/Collector.sol";
-import "../../scripts/collectors/defi/CustomOracle.sol";
 
-import "../../scripts/collectors/defi/protocols/AaveCollector.sol";
-import "../../scripts/collectors/defi/protocols/ERC20Collector.sol";
+import "../../scripts/collectors/defi/external/IAaveOracleV3.sol";
+import "../../scripts/collectors/oracles/PriceOracle.sol";
+
 import "forge-std/Script.sol";
 
 import {ArraysLibrary} from "../common/ArraysLibrary.sol";
 import {Constants} from "./Constants.sol";
 
+contract AaveOracle is ICustomPriceOracle {
+    uint256 public immutable Q96 = 2 ** 96;
+
+    address public aaveOracle;
+    address public asset;
+
+    constructor(address aaveOracle_, address asset_) {
+        aaveOracle = aaveOracle_;
+        asset = asset_;
+    }
+
+    function priceX96() public view returns (uint256) {
+        return IAaveOracleV3(aaveOracle).getAssetPrice(asset) * Q96;
+    }
+}
+
 contract Deploy is Script {
+    uint256 constant Q96 = 2 ** 96;
+    address public vault = 0x8769b724e264D38d0d70eD16F965FA9Fa680EcDe;
+
     function run() external {
         uint256 deployerPk = uint256(bytes32(vm.envBytes("HOT_DEPLOYER")));
         vm.startBroadcast(deployerPk);
-        address erc20Collector = address(new ERC20Collector());
-        address aaveCollector = address(new AaveCollector());
-        address[] memory protocols = new address[](4);
-        protocols[0] = address(erc20Collector);
-        protocols[1] = address(aaveCollector);
+        address deployer = vm.addr(deployerPk);
 
-        bytes[] memory protocolDeployments = new bytes[](2);
-        protocolDeployments[1] =
-            abi.encode(AaveCollector.ProtocolDeployment({pool: Constants.AAVE_CORE, metadata: "Core"}));
+        PriceOracle oracle = new PriceOracle(deployer);
+        PriceOracle.TokenOracle[] memory tokenOracles = new PriceOracle.TokenOracle[](5);
 
-        address[] memory assets = ArraysLibrary.makeAddressArray(
-            abi.encode(Constants.ETH, Constants.WETH, Constants.USDC, Constants.USDT, Constants.WBTC)
+        tokenOracles[0] = PriceOracle.TokenOracle({
+            constValue: 0,
+            oracle: address(new AaveOracle(Constants.AAVE_V3_ORACLE, Constants.WETH))
+        }); // MON
+        tokenOracles[1] = PriceOracle.TokenOracle({
+            constValue: 0,
+            oracle: address(new AaveOracle(Constants.AAVE_V3_ORACLE, Constants.WETH))
+        }); // WMON
+        tokenOracles[2] = PriceOracle.TokenOracle({
+            constValue: 0,
+            oracle: address(new AaveOracle(Constants.AAVE_V3_ORACLE, Constants.WBTC))
+        }); // WBTC
+        tokenOracles[3] = PriceOracle.TokenOracle({
+            constValue: 0,
+            oracle: address(new AaveOracle(Constants.AAVE_V3_ORACLE, Constants.USDC))
+        }); // USDC
+        tokenOracles[4] = PriceOracle.TokenOracle({
+            constValue: 0,
+            oracle: address(new AaveOracle(Constants.AAVE_V3_ORACLE, Constants.USDT))
+        }); // USDT
+
+        oracle.setOracles(
+            ArraysLibrary.makeAddressArray(
+                abi.encode(Constants.ETH, Constants.WETH, Constants.WBTC, Constants.USDC, Constants.USDT)
+            ),
+            tokenOracles
         );
 
-        CustomOracle impl = new CustomOracle(Constants.AAVE_V3_ORACLE, Constants.WETH);
+        Collector Impl = new Collector();
+        Collector collector = Collector(payable(new TransparentUpgradeableProxy(address(Impl), deployer, new bytes(0))));
+        collector.initialize(deployer, address(oracle));
+        vm.stopBroadcast();
+        console2.log("Collector", address(collector));
 
-        address o = Clones.cloneWithImmutableArgs(address(impl), abi.encode(protocols, protocolDeployments, assets));
+        tokenOracles = new PriceOracle.TokenOracle[](1);
+        tokenOracles[0] = PriceOracle.TokenOracle({constValue: 1e8 * Q96, oracle: address(0)}); // USD
+        oracle.setOracles(ArraysLibrary.makeAddressArray(abi.encode(collector.USD())), tokenOracles);
 
-        console2.log("erc20Collector:", erc20Collector);
-        console2.logBytes(erc20Collector.code);
-
-        console2.log("aaveCollector:", aaveCollector);
-        console2.logBytes(aaveCollector.code);
-
-        console2.log("impl:", address(impl));
-        console2.logBytes(address(impl).code);
-
-        console2.log("Collector", address(o));
-        console2.logBytes(o.code);
-        // CustomOracle(o).getDistributions(
-        //     0x277C6A642564A91ff78b008022D65683cEE5CCC5, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
-        // );
-        // console2.log("Collector: %s", address(collector));
-        // revert("ok");
-
-        // collector.collect(
-        //     0x0000000000000000000000000000000000000000,
-        //     Vault(payable(0x277C6A642564A91ff78b008022D65683cEE5CCC5)),
-        //     Collector.Config({
-        //         baseAssetFallback: address(0),
-        //         oracleUpdateInterval: 24 hours,
-        //         redeemHandlingInterval: 1 hours
-        //     })
-        // );
-        // revert("ok");
-
-        // address user = 0xE98Be1E5538FCbD716C506052eB1Fd5d6fC495A3;
-        // address vault = 0x277C6A642564A91ff78b008022D65683cEE5CCC5;
-
-        // collector.collect(
-        //     user,
-        //     Vault(payable(vault)),
-        //     Collector.Config({
-        //         baseAssetFallback: address(type(uint160).max / 0xf * 0xe),
-        //         oracleUpdateInterval: 24 hours,
-        //         redeemHandlingInterval: 1 hours
-        //     })
-        // );
-
-        // revert("ok");
-        // StrETHOracle.Balance[] memory b = strETHOracle.getDistributions(Vault(payable(vault)));
-        // for (uint256 i = 0; i < b.length; i++) {
-        //     console2.log(b[i].metadata, vm.toString(b[i].asset), vm.toString(b[i].balance));
-        // }
-
-        // console2.log(oracle.tvl(vault));
+        collector.collect(
+            address(0),
+            Vault(payable(vault)),
+            Collector.Config({
+                baseAssetFallback: Constants.WETH,
+                oracleUpdateInterval: 24 hours,
+                redeemHandlingInterval: 1 hours
+            })
+        );
         // revert("ok");
     }
 }
