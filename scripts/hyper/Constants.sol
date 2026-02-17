7f4bc75377fc949244081ab3d93b6761bef26046
Anton Iants <yants.anton@gmail.com>
2026-02-17 10:53:05 +0400

    updated constants

--- scripts/hyper/Constants.sol
+++ scripts/hyper/Constants.sol
@@ -12,13 +12,6 @@ library Constants {
     string public constant DEPLOYMENT_NAME = "Mellow";
     uint256 public constant DEPLOYMENT_VERSION = 1;
 
-    address public constant BTC = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address public constant MUSD = 0xdD468A1DDc392dcdbEf6db6e34E89AA338F9F186;
-    address public constant mcbBTC = 0x6a7CD8E1384d49f502b4A4CE9aC9eb320835c5d7;
-    address public constant mUSDC = 0x04671C72Aab5AC02A03c1098314b1BB6B560c197;
-    address public constant mUSDT = 0xeB5a5d39dE4Ea42C2Aa6A57EcA2894376683bB8E;
-    address public constant mUSDe = 0xdf6542260a9F768f07030E4895083F804241F4C4;
-
     function protocolDeployment() internal pure returns (ProtocolDeployment memory) {
         return ProtocolDeployment({
             deploymentName: DEPLOYMENT_NAME,
@@ -30,57 +23,59 @@ library Constants {
             symbioticFarmFactory: address(0),
             wsteth: address(0),
             weth: address(0),
-            proxyAdmin: 0xAFfAE6697Ca8B35f397FA2993B03cD9F506DdD17,
+            proxyAdmin: 0xb1E5a8F26C43d019f2883378548a350ecdD1423B,
             deployer: 0x4d551d74e851Bd93Ce44D5F588Ba14623249CDda,
-            factoryImplementation: Factory(0x00000008E7c244Fb6FA6Fc1fB5EC53Ec71c34386),
-            factory: Factory(0x000000071a219faa713E719F2DfB458b10dbAED1),
-            consensusFactory: Factory(0xE4Db00dCc29966368E8aA966ac75B6FE5B4113D7),
-            depositQueueFactory: Factory(0x664B70AE0a01D9beF57c2Eb64664B0CFB055A461),
-            redeemQueueFactory: Factory(0x7f2F6B155B41F2DD8A8Ea0FbA3b4EA1ceDc6260e),
-            feeManagerFactory: Factory(0x4b2b12a33e260ef35e84687860785d263EfF5172),
-            oracleFactory: Factory(0xbb7e4da67Fe8E66AB4AE8EAb2999e731DA364492),
-            riskManagerFactory: Factory(0x902266fE38DD8Eee290987490bD57537c82007a1),
-            shareManagerFactory: Factory(0x6C3BB5478fD189DCf35Fb3a4b56015163392AA35),
-            subvaultFactory: Factory(0x3B62CaA341fA0535a479B394c2f6EA28ee8fA449),
-            vaultFactory: Factory(0x5310AD84B0cd3Af376C751EAb81ceE414bD442d8),
-            verifierFactory: Factory(0xbc1468D587DaEE3023E2b41Cc642643AF3221178),
-            erc20VerifierFactory: Factory(0x0d634B6e35368b8954C53b38aDF72716a16667FA),
+            factoryImplementation: Factory(0x000000092C4e111CBA592380b258d94B37038B63),
+            factory: Factory(0x00000003cEe0DbFb61dD598CD7978993A37f8F8C),
+            consensusFactory: Factory(0x4C496F31a4D46044E57214f282420b8b078edf56),
+            depositQueueFactory: Factory(0x66B1a68F8CE628d508290d5C1d74Bc50416BDF90),
+            redeemQueueFactory: Factory(0x0a69B47c3E0bD7e5B1E1Db95d6C0b2914607e19f),
+            feeManagerFactory: Factory(0x71a4D9739A35B4e86118F3a45bae662Bcc9357FA),
+            oracleFactory: Factory(0xe4E2b5Db061A731D96b9267464c17Ba282326Ce7),
+            riskManagerFactory: Factory(0x95ff5434A51f3E42fCeD2Cae36548d95e56bAb10),
+            shareManagerFactory: Factory(0x3755c140b90dC4E6b1A6361279B2C2eCc0358689),
+            subvaultFactory: Factory(0x72244F91242244E62Af3417294B828E262EbdfE7),
+            vaultFactory: Factory(0xC7332ab052350Bbb9075f1160cc7073428981638),
+            verifierFactory: Factory(0x40383d404e570D95fF68945d2a334fb2f5ecE0f6),
+            erc20VerifierFactory: Factory(0x7025132709b3B01D663D97e56eae37988471c75a),
             symbioticVerifierFactory: Factory(address(0)),
             eigenLayerVerifierFactory: Factory(address(0)),
             swapModuleFactory: Factory(address(0)),
-            consensusImplementation: Consensus(0x000000083a1bE8Aa2Aa5fB244c84A6E410e6ce24),
-            depositQueueImplementation: DepositQueue(payable(0x0000000518eC830D8C3da6056b34A0dfBF9e924d)),
-            syncDepositQueueImplementation: SyncDepositQueue(payable(address(0x0000000B813e85943D42c5187efAb487E12e1485))),
-            signatureDepositQueueImplementation: SignatureDepositQueue(payable(0x00000006A03A937E4B316F02a5130e4FB0B22Dea)),
-            redeemQueueImplementation: RedeemQueue(payable(0x0000000B04E01e31715f4137D664aB402268B9d0)),
-            signatureRedeemQueueImplementation: SignatureRedeemQueue(payable(0x000000004a3F4ff856e7cb47A0ae8aDe6d133cFB)),
-            feeManagerImplementation: FeeManager(0x0000000df5Cff487723b8D8c58eD5C336d8a2317),
-            oracleImplementation: Oracle(0x0000000C705B7C7485F62Bc1DF7554fD6EB6C602),
-            riskManagerImplementation: RiskManager(0x0000000A7a10ea335C54E03220cfAe92310b2465),
-            tokenizedShareManagerImplementation: TokenizedShareManager(0x000000071F09E877c469749c093d09FB17896D6c),
-            basicShareManagerImplementation: BasicShareManager(0x00000008be96121073931e2b6Da8f5711a52097d),
-            subvaultImplementation: Subvault(payable(0x0000000585bE8a415f9edCdC3C56472625BB2E02)),
-            verifierImplementation: Verifier(0x000000097bD869258523A17D1e9836E71Ef8aB2A),
-            vaultImplementation: Vault(payable(0x0000000B39b91D795b9975219E228bCb4D33A6A3)),
-            bitmaskVerifier: BitmaskVerifier(0x0000000819BA998E0Dfe0DAfdd6B23dBf103314D),
+            accountFactory: Factory(0xCda7916AA830B4dAb8295FBa92953d5251f5FDFa),
+            consensusImplementation: Consensus(0x00000008086A535Febd23fBd8C8F7d9D987930B7),
+            depositQueueImplementation: DepositQueue(payable(0x0000000A151048f4f01996a9Cd35a982F5830251)),
+            syncDepositQueueImplementation: SyncDepositQueue(payable(0x00000007d43702d556707a63132d42BcDf47E7dD)),
+            signatureDepositQueueImplementation: SignatureDepositQueue(payable(0x00000003e7D5d1EDF85b03b974aAc374d0FCB8A1)),
+            redeemQueueImplementation: RedeemQueue(payable(0x00000002F8d3f0D03E9Ce461791F6A0a9d28D0f6)),
+            signatureRedeemQueueImplementation: SignatureRedeemQueue(payable(0x000000047f8812704050cB86E549Fe8f28512A2D)),
+            feeManagerImplementation: FeeManager(0x00000003bf6bEC83fA8ff147b04176B82F591497),
+            oracleImplementation: Oracle(0x00000001bdbaFbE0Fb55b7d74a6dB74D1DA6047E),
+            riskManagerImplementation: RiskManager(0x0000000a0d139B4B7add54D70e2a4ED3c81C513C),
+            tokenizedShareManagerImplementation: TokenizedShareManager(0x0000000f2a485f26efd108144cCBFc46b18cB3e0),
+            basicShareManagerImplementation: BasicShareManager(0x0000000454d68af6Faf344e8acAa372f136749c5),
+            subvaultImplementation: Subvault(payable(0x00000008c8A185371Ab8eB28bbdb875cd526B69C)),
+            verifierImplementation: Verifier(0x00000007eEEbCA71f6b261061136BaFA666218A5),
+            vaultImplementation: Vault(payable(0x00000002334dBFa3B92467eA9Eb970ec1e067377)),
+            bitmaskVerifier: BitmaskVerifier(0x0000000f7DA5A9480262Ac3D654b1F4aA9F604B8),
             eigenLayerVerifierImplementation: EigenLayerVerifier(address(0)),
-            erc20VerifierImplementation: ERC20Verifier(0x000000038Cd2281fe3C651A8B9C2380Ea15f2c87),
+            erc20VerifierImplementation: ERC20Verifier(0x000000010849B881DA846FFEb1078A433284F8D0),
             symbioticVerifierImplementation: SymbioticVerifier(address(0)),
-            vaultConfigurator: VaultConfigurator(0x00000000f731118c52AeA768c1ac22CEcA7e3b8D),
-            basicRedeemHook: BasicRedeemHook(0x0000000887657b16F0dc7EFbb2be9EA77cEDF16c),
-            redirectingDepositHook: RedirectingDepositHook(0x0000000B77FC23f6F0f4c51238D6e1c76DefBFdb),
+            vaultConfigurator: VaultConfigurator(0x00000003986f4F63CdBAB0f5d78fff57495fee85),
+            basicRedeemHook: BasicRedeemHook(0x000000033FDa28b7025Fb16D53F81c3C1F78d572),
+            redirectingDepositHook: RedirectingDepositHook(0x0000000Cbd64305e1668dB5F8a542c2c7EC61640),
             lidoDepositHook: LidoDepositHook(address(0)),
-            oracleHelper: OracleHelper(0x00000002FC616d31133ab9AD626E43a94674D5B6),
-            swapModuleImplementation: SwapModule(payable(address(0)))
+            oracleHelper: OracleHelper(0x00000005dc87A3230E0F3195C5e9220DCFF1E182),
+            swapModuleImplementation: SwapModule(payable(address(0))),
+            mellowAccountV1Implementation: MellowAccountV1(0x00000001f879d5dAE0066E714867014Ec265F4ab)
         });
     }
 
     OracleSubmitterFactory public constant oracleSubmitterFactory =
-        OracleSubmitterFactory(0x00000007AA9Bd15F538a2d1D68A2aCFE8D09BFd0);
+        OracleSubmitterFactory(0x0000000dB76510D5B4D99df16160469bF782B227);
 
     DeployVaultFactoryRegistry public constant deployVaultFactoryRegistry =
-        DeployVaultFactoryRegistry(0x00000002650310C073F90341C2C272aF5C56e292);
+        DeployVaultFactoryRegistry(0x00000008656A21E6f690d40BAc97736f62E54853);
 
     DeployVaultFactory public constant deployVaultFactory =
-        DeployVaultFactory(0x000000007f271A7C5E6C928B7998efc3b8Dbc5EF);
+        DeployVaultFactory(0x00000005b5Dda102b4F9104fE8c537f816ac76D4);
 }
