56c17bc719b265a6286ec7e2ee2a3e85c68b949f
Anton Iants <yants.anton@gmail.com>
2026-07-06 14:15:03 +0400

    add Arc mainnet deployment script

---
+++ scripts/arc/Constants.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import "../common/Permissions.sol";
+import "../common/ProofLibrary.sol";
+import "../common/interfaces/Imports.sol";
+
+library Constants {
+    address public constant USDC = 0x3600000000000000000000000000000000000000;
+}
---
+++ scripts/arc/Deploy.s.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.25;
+
+import {Constants} from "./Constants.sol";
+import {Script} from "forge-std/Script.sol";
+
+import {AcceptanceLibrary} from "../common/AcceptanceLibrary.sol";
+import {ArraysLibrary} from "../common/ArraysLibrary.sol";
+import {ProtocolDeployment, ProtocolDeploymentLibrary} from "../common/ProtocolDeploymentLibrary.sol";
+
+contract TestVerifContract {
+    function test() external pure returns (uint256) {
+        return 42;
+    }
+}
+interface ISafe{
+    function createProxyWithNonce(
+        address _singleton,
+        bytes memory initializer,
+        uint256 saltNonce
+    ) external returns (address proxy);
+}
+
+contract Deploy is Script {
+    function run() external {
+        uint256 deployerPk = uint256(bytes32(vm.envBytes("HOT_DEPLOYER")));
+        address deployer = vm.addr(deployerPk);
+
+        address proxyAdmin = 0x81698f87C6482bF1ce9bFcfC0F103C4A0Adf0Af0;
+
+        vm.startBroadcast(deployerPk);
+
+        new TestVerifContract();
+        return;
+
+        ProtocolDeployment memory deployment = ProtocolDeploymentLibrary.deploy(
+            deployer,
+            proxyAdmin,
+            ProtocolDeploymentLibrary.DeploymentParams({
+                cowswapSettlement: address(0),
+                cowswapVaultRelayer: address(0),
+                weth: address(0),
+                minLeadingZeros: 8,
+                salt: ArraysLibrary.makeBytes32Array(
+                    abi.encode(
+                        [
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3c84825df801fe3cddd000018,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a31fb8c57e5408f59f9b0100a8,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3dbd67913fc42eb9f5a040098,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3253648a35e86bb3769030024,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3eea2af40b4aadd8ee5048042,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3dac10345abeee55bbe060050, // fixed SyncDepositQueue
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a33a4f2e40558ea2c8680b000c
+                        ],
+                        [
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a33f3964b9658a37a1521c00c0,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3c381555325c0f512de020052,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a39d3fdfb6ba68aafba2070050,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a325a418919c68f29ffd060040,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a315de2307b2dc0a3b77020083,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a32edf144ba8e329a1c40c00c0
+                        ],
+                        [
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a341f6452bbf54509949090010,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a34cfc13261c78fa65f9140040,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3e28386d1049f1bd0e0016002,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3be5325ebffb99ef295040050,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3b9749c1f9b20d6de9d000030
+                        ],
+                        [
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a377b984851164fb24773da182, // 0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3ec5e4cf2261f94efc60500a8
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a327064c975c06dbe66a1f4044,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3d49a92b364e8d5272505000d,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a31153e95f02680f466007000a
+                        ],
+                        [
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3ce2a1d03aa7114825a0a0040,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a3d318478af05f992e29068428,
+                            0xe98be1e5538fcbd716c506052eb1fd5d6fc495a37ad1425a7b9c5d20490f0026
+                        ]
+                    )
+                )
+            })
+        );
+
+        vm.stopBroadcast();
+
+        AcceptanceLibrary.runProtocolDeploymentChecks(deployment);
+
+        revert("deployment complete");
+    }
+}
